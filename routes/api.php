2cd03d6238273cf02e1fdb7d8c5e55773ec0135c
Jugg1 <adeoluwaadeboye@gmail.com>
2021-08-27 08:19:26 +0100

    :construction: Added a Test1 Endpoint

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get("/test", function (Request $request) {
+    return "Flex";
+    //for reasons i don't know you have sent "Accept: application/json" on POSTMAN to get a good response
+});
